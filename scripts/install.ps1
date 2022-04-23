25299f050367a7ef8c3c38621af506622d4356b5
ironyman <ironyman13@gmail.com>
2022-04-23 01:37:22 -0700

    Improve scheduled task

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -23,11 +23,17 @@ $taskTrigger = New-ScheduledTaskTrigger -atlogon -User $env:USERNAME
 # Required to set_foreground_window_terminal on high integrity level process windows.
 $principal = New-ScheduledTaskPrincipal -UserId $env:USERNAME -RunLevel Highest
 
+# For some reason taskscheduler thinks QuakeRun is still running long after noconsole.exe or quakerun.exe exits.
+# Adding -ExecutionTimeLimit 1 means taskscheduler will assume QuakeRun already exited and quakerun.exe
+# has its own mechanism for ensuring only one instance is running.
+$setting = New-ScheduledTaskSettingsSet -MultipleInstances Parallel -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -ExecutionTimeLimit 1
+
 Register-ScheduledTask `
     -TaskName QuakeRun `
     -Action $taskAction `
     -Trigger $taskTrigger `
-    -principal $principal `
+    -Principal $principal `
+    -Setting $setting `
     -Description "Start switch.exe with quakerun.exe at logon" `
     -force
 
