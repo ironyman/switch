56a282aa889396862e527847b4b6bfb858623880
ironyman <ironyman13@gmail.com>
2022-08-10 16:34:46 -0700

    Update choco package version

--- choco/update.ps1
+++ choco/update.ps1
@@ -5,16 +5,16 @@ $files = (
     ".\scripts\uninstall.ps1",
     ".\target\release\noconsole.exe",
     ".\target\release\quakerun.exe",
-    ".\target\release\switch.exe"
+    ".\target\release\switch.exe",
     ".\target\release\indexer.exe"
 )
 $unshim = (
     "$PSScriptRoot\tools\noconsole.exe.ignore",
-    "$PSScriptRoot\tools\switch.exe.ignore"
+    "$PSScriptRoot\tools\switch.exe.ignore",
     "$PSScriptRoot\tools\indexer.exe.ignore"
 )
 
-rm -recurse -force $PSScriptRoot\tools\
+rm -recurse -force $PSScriptRoot\tools\ -ea ignore
 new-item -type directory $PSScriptRoot\tools\ -force | out-null
 copy $PSScriptRoot\pre_tools\* $PSScriptRoot\tools\ -recurse -force
 copy $files $PSScriptRoot\tools
